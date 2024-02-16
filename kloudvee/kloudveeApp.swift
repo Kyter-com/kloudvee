9451bdab84e2ae5fc349f0a76fcd1ccc23840294
Nick Reisenauer <nickdreisenauer@gmail.com>
2024-02-16 14:21:37 -0700

    Initial Commit

---
+++ kloudvee/kloudveeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kloudveeApp.swift
+//  kloudvee
+//
+//  Created by Nick Reisenauer on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct kloudveeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
